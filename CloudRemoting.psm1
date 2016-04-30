0013c90009c1012bff6f2369e3f122b3b4ec5235
Akos Murati <akos@murati.hu>
2016-04-30 17:17:53 +1200

    Fix AWSPowershell dependency

--- CloudRemoting.psm1
+++ CloudRemoting.psm1
@@ -1,5 +1,12 @@
 Write-Verbose "CloudRemoting module"
 
+if (Get-Module AWSPowershell -ListAvailable) {
+    Import-Module AWSPowershell
+} else {
+    Write-Warning "AWSPowershell is not found, but some of the cmdlets requires it."
+    Write-Warning "Please make sure you have installed it for proper functioning."
+}
+
 $functionFilter = Join-Path $PSScriptRoot "Functions\*.ps1"
 Get-ChildItem -Path $functionFilter -Recurse | Foreach-Object {
     Write-Verbose "Loading file $($_.Name).."
