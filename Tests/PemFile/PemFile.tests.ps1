6d6f081b98b0885f0ab4da65a8267191958021ab
Akos Murati <akos@murati.hu>
2016-09-17 05:52:59 +1200

    Add new Validation tests

---
+++ Tests/PemFile/PemFile.tests.ps1
@@ -0,0 +1,41 @@
+. (Join-Path $PSScriptRoot '../TestCommon.ps1')
+
+Describe "Validation - Default EC2 PemFile is not set validation" {
+    $fakeInstance = 'i-123456'
+    $fakeRegion = 'us-east-1'
+    $emptyFile = Resolve-Path(Join-Path $PSScriptRoot '../PemFile/empty.txt')
+
+    function Test-PemFileValidation([string]$FunctionName) {
+        Context $FunctionName {
+            it "should throw error if not specified" {
+                $test = { . $FunctionName -InstanceId $fakeInstance -Region $fakeRegion }
+                $test | Should Throw $exceptionText
+            }
+
+            $exceptionText = 'Provide an argument that is not null or empty'
+            foreach($case in @($null, '')) {
+                it "should throw '$exceptionText' if '$case' passed" {
+                    $test = { . $FunctionName -InstanceId $fakeInstance -Region $fakeRegion -PemFile $case }
+                    $test | Should Throw $exceptionText
+                }
+            }
+
+            $exceptionText = 'Please provide the path to a valid PemFile.'
+            foreach($case in @('x:\unlikelytoexists',$emptyFile)) {
+                it "should throw '$exceptionText' if '$case' passed" {
+                    $test = { . $FunctionName -InstanceId $fakeInstance -Region $fakeRegion -PemFile $case }
+                    $test | Should Throw $exceptionText
+                }
+            }
+        }
+    }
+
+    @(
+        'Get-Ec2Credential'
+        'New-EC2PSSession'
+        'Enter-EC2PSSession'
+        'Enter-EC2RDPSession'
+    ) | ForEach-Object {
+        Test-PemFileValidation -FunctionName $_
+    }
+}
