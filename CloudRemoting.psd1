b326c14e6c65a77387269118d2d9dad1f62a4b8b
Akos Murati <akos@murati.hu>
2016-11-24 13:39:48 +1300

    Add Expand-Object to module

--- CloudRemoting.psd1
+++ CloudRemoting.psd1
@@ -10,7 +10,7 @@
 RootModule = 'CloudRemoting.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.5.3.0'
+ModuleVersion = '0.6.0.0'
 
 # ID used to uniquely identify this module
 GUID = 'd58ec090-b8b8-40ee-8292-049de3f17f4c'
@@ -88,6 +88,8 @@ FunctionsToExport = @(
     'Set-DefaultSSMOutput'
     'Clear-DefaultSSMOutput'
     'Invoke-SSMCommand'
+
+    'Expand-Object'
 )
 
 # Cmdlets to export from this module
---
+++ Functions/Expand-Object.ps1
@@ -0,0 +1,53 @@
+﻿<#
+.SYNOPSIS
+    Enriches the inputobject, by expanding properties to the top
+    level.
+.DESCRIPTION
+    Expands the InputObject with the -ExpandProperty and merges its
+    content to the top level.
+
+.PARAMETER InputObject
+    Mandatory - InputObject to be expanded
+.PARAMETER Force
+    Optional - Switch to to override properties that already exist
+
+.EXAMPLE
+    Invoke-SSMCommand { Get-Date } -EnableCliXml | Expand-Object -Force
+#>
+function Expand-Object {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [PsObject[]]$InputObject,
+
+        [Parameter()]
+        [Alias('Property')]
+        [string[]]$ExpandProperty=@('Tags','SSMCommandInputObject'),
+
+        [Parameter()]
+        [switch]$Force
+    )
+
+    process {
+        $InputObject |
+        ForEach-Object {
+            foreach ($expandable in ($_| Get-Member -Name $ExpandProperty | Select-Object -ExpandProperty Name)) {
+                Write-Verbose "Processing '$expandable'.."
+                if ($_.$expandable | Get-Member -Name @('Key','Value') -ErrorAction SilentlyContinue) {
+                    Write-Verbose "Hash expansion"
+                    foreach($enty in $_.$expandable) {
+                        $_ | Add-Member -NotePropertyName $enty.Key -NotePropertyValue $enty.Value -Force:$Force
+                    }
+                }
+                elseif ($_.$expandable) {
+                    Write-Verbose "PSObject expansion"
+                    foreach($property in ($_.$expandable | Get-Member -MemberType Properties)) {
+                        $_ | Add-Member -NotePropertyName $property.Name -NotePropertyValue $_.$expandable.($Property.Name) -Force:$Force
+                    }
+                }
+
+                $_
+            }
+        }
+    }
+}
