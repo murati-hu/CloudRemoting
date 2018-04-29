ff518f457df0e05104899d48729b5b57b823f0e9
Ákos Muráti <akos@murati.hu>
2018-04-29 13:58:46 +1200

    Remove AWSPowershell mock from pester, since binary mocks not supported anymore on .NET Core

--- Tests/PemFile/PemFile.tests.ps1
+++ Tests/PemFile/PemFile.tests.ps1
@@ -4,7 +4,7 @@ Describe "Validation - No Default EC2 PemFile" {
     $fakeInstance = 'i-123456'
     $fakeRegion = 'us-east-1'
     $emptyFile = Resolve-Path(Join-Path $PSScriptRoot '../PemFile/empty.txt')
-    
+
     function Test-PemFileValidation([string]$FunctionName) {
         Context $FunctionName {
             it "should throw error if not specified" {
@@ -38,31 +38,5 @@ Describe "Validation - No Default EC2 PemFile" {
         'Enter-EC2RDPSession'
     ) | ForEach-Object {
         Test-PemFileValidation -FunctionName $_
-    }
-}
-
-Describe "Validation - With Default EC2 PemFile set" {
-    $fakeInstance = 'i-123456'
-    $fakeRegion = 'us-east-1'
-    $notemptyFile = Resolve-Path(Join-Path $PSScriptRoot '../PemFile/notempty.txt')
-
-    function Test-DefaultPemFile([string]$FunctionName) {
-        Context $FunctionName {
-            $exceptionText = 'Please provide the path to a valid PemFile.'
-            it "$FunctionName should NOT throw '$exceptionText' if not specified" {
-                $test = { . $FunctionName -InstanceId $fakeInstance -Region $fakeRegion }
-                $test | Should Not Throw $exceptionText
-            }
-        }
-    }
-
-    Set-DefaultEC2PemFile -PemFile $notemptyFile
-    @(
-        'Get-Ec2Credential'
-        'New-EC2PSSession'
-        'Enter-EC2PSSession'
-        'Enter-EC2RDPSession'
-    ) | ForEach-Object {
-        Test-DefaultPemFile -FunctionName $_
     }
 }
